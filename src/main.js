f2ac18e248774b8b7fb15195e1bacd3033fdb65d
Abhay Yadav <develop.inside.india@gmail.com>
2020-08-28 19:59:04 +0530

    Left Navigation Menu FIx
    Need to work on top bar.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import './styles/styles.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VueSidebarMenu)
 
 new Vue({
   render: h => h(App),
